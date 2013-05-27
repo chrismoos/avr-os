5c94d5f98e1852a0789f05b8cf4bd554769e086e
Chris Moos <chris@tech9computers.com>
2013-05-27 10:59:52 -0700

    travis-ci: try again

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -9,4 +9,3 @@ cd simavr && sudo make install
 cd ../
 wget -O "arduino.tar.gz" "http://arduino.googlecode.com/files/arduino-1.0.3-linux64.tgz"
 tar xzf arduino.tar.gz
-export AVR_HOME="$(pwd)/arduino-1.0.3"
