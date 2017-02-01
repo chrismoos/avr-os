67dba2b9bccd730d1905229e19f95cb50ccaaac8
Chris Moos <chris@chrismoos.com>
2017-01-31 22:21:15 -0700

    Fix link to Arduino for tests.

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -7,5 +7,5 @@ cd build
 git clone https://github.com/buserror/simavr
 cd simavr && sudo make install
 cd ../
-wget -O "arduino.tar.gz" "http://arduino.googlecode.com/files/arduino-1.0.3-linux64.tgz"
+wget -O "arduino.tar.gz" "http://downloads.arduino.cc/arduino-1.0.3-linux64.tgz"
 tar xzf arduino.tar.gz
